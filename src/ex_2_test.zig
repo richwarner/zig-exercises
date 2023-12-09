a2fe10400e6766a0405a6ab0d3c3fe3cb8357897
Rich Warner <richwarneriii@gmail.com>
2023-12-09 19:29:26 +0000

    fix file names

--- src/ex_2_test.zig
+++ src/ex_2_test.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const ex_1 = @import("ex_2_solution.zig");
+const ex_1 = @import("ex_2.zig");
 
 test "expect addOne adds one to 41" {
     try std.testing.expect(ex_1.addOne(41) == 42);
